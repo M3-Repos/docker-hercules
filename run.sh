cb1bf7507a16a71ada5f3b311014cf54a7dbbbb9
Jonathan Shields <jshieldsf@gmail.com>
2025-06-26 18:36:49 -0400

    Standerdize image name

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -d -u root -p 3270:3270 -p 8038:8038 -v $(pwd)/MAINFRAME:/home/hercules/MAINFRAME mainframe-container:latest
+docker run -d -u root -p 3270:3270 -p 8038:8038 -v $(pwd)/MAINFRAME:/home/hercules/MAINFRAME m3-repos/hercules:latest
