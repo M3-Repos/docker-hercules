1d79c0d879ee5f5393176c262b678e0ee5beab32
RedSummit1 <jshieldsf@gmail.com>
2025-07-03 16:45:41 -0400

    Explicitly bind to root host loopback address at port 3270

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -d -u root -p 3270:3270 -p 8038:8038 -v $(pwd)/MAINFRAME:/home/hercules/MAINFRAME m3-repos/hercules:latest
+docker run -d -u root -p 127.0.0.1:3270:3270 -p 8038:8038 -v $(pwd)/MAINFRAME:/home/hercules/MAINFRAME m3-repos/hercules:latest
